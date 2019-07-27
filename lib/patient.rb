c1c9936c29a60052ba85272c7eb4abd68ae43355
Jaime Rodas <jaime@rodas.mx>
2019-07-27 12:22:19 -0500

    Mejora esqueleto de Patient
    - Ya sabemos qué forma va a tomar el objeto de respuesta.
    - Devuelve un mock del objeto para los id's válidos.

--- lib/patient.rb
+++ lib/patient.rb
@@ -4,12 +4,35 @@ class Patient
   end
 
   def initialize(id)
+    @id = id.to_i
   end
 
+  attr_reader :id
+
   def exists?
-    false
+    true
   end
 
   def record
+    [
+      200,
+      {"Content-Type" => "application/json"},
+      {
+        codigo: 200,
+        mensaje: "Petición completada.",
+        payload: response_object,
+      }.to_json,
+    ]
+  end
+
+  def response_object
+    {
+      no_expediente: id,
+      fecha_ultima_consulta: "31/12/19",
+      tipo_sangre: "A+",
+      alergias: [
+        {nombre: "algo", fecha_alta: "31/12/19", medicamento: "algo"},
+      ],
+    }
   end
 end
